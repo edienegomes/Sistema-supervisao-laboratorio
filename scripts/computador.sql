1e16835f5dae9f2c1f8a889879e220d750561a41
Felipe Feliciano Lopes <felicianotaco133@gmail.com>
2026-06-07 22:02:45 -0300

    alterções nos atributos

--- scripts/computador.sql
+++ scripts/computador.sql
@@ -5,5 +5,15 @@ CREATE TABLE COMPUTADOR(
 	FOREIGN KEY (id_dispo) REFERENCES DISPOSITIVO (id)
 );
 
-
+INSERT INTO computador (id_dispo,sistema_operacional)
+values
+('440.702.000387.2024','Windows 11',),
+('440.702.000360.2024','Windows 11'),
+('440.702.000368.2024','Windows 11'),
+('440.702.000370.2024','Windows 11'),
+('440.702.000392.2024','Windows 11'),
+('440.702.000358.2024','Windows 11'),
+('440.702.000375.2024','Windows 11'),
+('440.702.000385.2024','Windows 11'),
+('440.702.000365.2024','Windows 11');
 
