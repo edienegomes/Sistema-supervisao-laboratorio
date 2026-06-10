91b7e60ddbe15060e29075e377925cdd26872063
João Barreto <Anothermailb@gmail.com>
2026-06-09 23:49:35 -0300

    Update verifica.sql

--- scripts/verifica.sql
+++ scripts/verifica.sql
@@ -2,9 +2,7 @@ CREATE TABLE VERIFICA(
 	matricula INT,
 	id VARCHAR(30),
 	funcionamento VARCHAR(30) NOT NULL,
-	
 	PRIMARY KEY (matricula, id),
-	
 	FOREIGN KEY (matricula) REFERENCES MONITOR (matricula),
 	FOREIGN KEY (id) REFERENCES DISPOSITIVO (id)
 );
