2e0a7f756e46075d81e78c146dc0a1364194dcf6
João Barreto <Anothermailb@gmail.com>
2026-06-13 16:57:23 -0300

    Delete scripts/verifica.sql

--- scripts/verifica.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE VERIFICA(
-	matricula INT,
-	id VARCHAR(30),
-	funcionamento VARCHAR(30) NOT NULL,
-	PRIMARY KEY (matricula, id),
-	FOREIGN KEY (matricula) REFERENCES MONITOR (matricula),
-	FOREIGN KEY (id) REFERENCES DISPOSITIVO (id)
-);
