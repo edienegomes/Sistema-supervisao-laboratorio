92886efb1d36dd7b1e73e6b7753c0ec4f5daa0d9
João Barreto <Anothermailb@gmail.com>
2026-06-13 16:58:31 -0300

    Delete scripts/monitora.sql

--- scripts/monitora.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE MONITORA(
-	id_laboratorio INT,
-	matricula INT,
-	PRIMARY KEY (id_laboratorio, matricula),
-	FOREIGN KEY (id_laboratorio) REFERENCES LABORATORIO (id_laboratorio),
-	FOREIGN KEY (matricula) REFERENCES MONITOR (matricula)
-);
