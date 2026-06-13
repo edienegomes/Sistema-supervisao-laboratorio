c44d629dafbd44b43a93233bff4d8524ed487133
João Barreto <Anothermailb@gmail.com>
2026-06-13 17:20:22 -0300

    Update popular_tabelas.sql

--- scripts/popular_tabelas.sql
+++ scripts/popular_tabelas.sql
@@ -1,10 +1,10 @@
 INSERT INTO monitor (matricula,primeiro_nome,sobrenome, turno)
 VALUES
-('200123435','Carlos','Miguel','Diurno'),
+('200123435','Carlos','Melo','Diurno'),
 ('200126767','Vinicius','Carnaíba','Noturno');
 
-INSERT INTO laboratorio (id_laboratorio,capacidade,bloco)
-VALUES (3,35,'bloco2');
+INSERT INTO LABORATORIO (id_laboratorio,capacidade,bloco)
+VALUES (2, 30, 'bloco2');
 
 --Tabela associativa
 INSERT INTO monitora (id_laboratorio, matricula)
@@ -13,8 +13,18 @@ VALUES
 (2,'200126767');
 
 --valores 
-INSERT INTO dispositivo (id,nome,status,id_lab)
-VALUES
+INSERT INTO DISPOSITIVO (id, nome, status, id_lab)
+VALUES 
+('000007.2021', 'computador', 'funcionando', 2),
+('000008.2021', 'computador', 'funcionando', 2),
+('000009.2021', 'computador', 'funcionando', 2),
+('000011.2021', 'computador', 'funcionando', 2),
+('000014.2021', 'computador', 'funcionando', 2),
+('000022.2021', 'computador', 'funcionando', 2),
+('000024.2021', 'computador', 'funcionando', 2),
+('000025.2021', 'computador', 'funcionando', 2),
+('000028.2021', 'computador', 'funcionando', 2),
+('000029.2021', 'computador', 'funcionando', 2);
 
 
 --Tabela associativa
@@ -29,18 +39,18 @@ VALUES
 ('1003','memória RAM','interno','440710.000440.2025'),
 ('1001','mouse','externo','440710.000442.2025');
 
-INSERT INTO computador (id_dispo, sistema_operacional)
+INSERT INTO COMPUTADOR (id_dispo,sistema_operacional)
 VALUES
-('440710.000440.2025','Windows 11'),
-('440710.000442.2025','Windows 11'),
-('440710.000454.2025','Windows 11'),
-('440710.000457.2025','Windows 11'),
-('440710.000465.2025','Windows 11'),
-('440710.000466.2025','Windows 11'),
-('440710.000475.2025','Windows 11'),
-('440710.000481.2025','Windows 11'),
-('440710.000482.2025','Windows 11'),
-('440710.000483.2025','Windows 11');
+('000007.2021','Windows 11'),
+('000008.2021','Windows 11'),
+('000009.2021','Windows 11'),
+('000011.2021','Windows 11'),
+('000014.2021','Windows 11'),
+('000022.2021','Windows 11'),
+('000024.2021','Windows 11'),
+('000025.2021','Windows 11'),
+('000028.2021','Windows 11'),
+('000029.2021','Windows 11');
 
 INSERT INTO reproducao (Id_dispo)
 ('440000.00010.2025');
